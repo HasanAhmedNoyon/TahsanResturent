d4f34d616178e30787cdb0118453e0f93f80309a
HasanAhmedNoyon <mohammedhasan9047@gmail.com>
2020-03-30 23:51:18 +0600

    First commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".toggle1").click(function () {
+        $("#navbarNav1").slideToggle("slow");
+        
+    });
+     $(".toggle2").click(function () {
+        $("#navbarNav2").slideToggle("slow");
+        
+    });
+    
+    
+    
+});
